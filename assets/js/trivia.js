c727b16a53c5131bc46908d9c2418d01b1a7a489
Mike Gross <grossmi4@gmail.com>
2018-09-30 11:50:34 -0400

    converted logic to object based structure

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,39 +1,44 @@
-const queryUrl = 'https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple';
+const
 
-let questionReturn = function() {
-  $.ajax({
-    url: queryUrl,
-    method: "GET",
-  }).then(response => {
-    console.log(response);
-    let answers = [];
-    let results = response.results[0];
-    answers.push(results.correct_answer);
-    results.incorrect_answers.forEach( answer => {
-      answers.push(answer)
+triviaAPI = {
+  queryUrl:'https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple',
+  questionReturn: function () {
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(response => {
+      console.log(response);
+      let answers = [];
+      let results = response.results[0];
+      answers.push(results.correct_answer);
+      results.incorrect_answers.forEach(answer => {
+        answers.push(answer)
+      });
+      triviaAPI.shuffle(answers);
+      console.log(results.correct_answer);
+      console.log(answers);
+      triviaAPI.displayQ(results.question, answers);
     });
-    shuffle(answers);
-    console.log(results.correct_answer);
-    console.log(answers);
-    displayQ(results.question,answers);
-  });
+  },
+
+  shuffle: function (a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
+  },
+
+  displayQ: function(question,answers) {
+    $("#question").html(`<h4>${question}</h4>`);
+    $("#answer1").text(answers[0]);
+    $("#answer2").text(answers[1]);
+    $("#answer3").text(answers[2]);
+    $("#answer4").text(answers[3]);
+  },
 };
 
-let displayQ = function(question,answers) {
-  $("#question").html(`<h4>${question}</h4>`);
-  $("#answer1").text(answers[0]);
-  $("#answer2").text(answers[1]);
-  $("#answer3").text(answers[2]);
-  $("#answer4").text(answers[3]);
-};
 
-let shuffle =function (a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
-  }
-  return a;
-};
