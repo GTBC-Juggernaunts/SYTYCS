558a0a395c047ae6c309b875dd09dc45697be9e7
Peter Huynh <huynhxpeter@gmail.com>
2018-10-01 23:29:47 -0400

    converted authentication into object

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -8,61 +8,74 @@ const config = {
   messagingSenderId: "15638323154"
 };
 firebase.initializeApp(config);
-const auth = firebase.auth();
 
 // create instance of Google provider object
+const auth = firebase.auth();
 const provider = new firebase.auth.TwitterAuthProvider();
 
-// signout
+// grab login and logout buttons
 const logoutBtn = document.getElementById("logout");
 const loginBtn = document.getElementById("login");
 
+// database object
+const firebaseAuth = {
+  signOut: () => {
+    auth.signOut();
+  },
+  signIn: () => {
+    auth
+      .signInWithRedirect(provider)
+      .them(result => {
+        // this gives you a google access token. Used to access the google api
+        const token = result.credential.accessToken;
+        // the signed-in user info
+        const user = result.user;
+        console.log(`user: ${user}`);
+        console.log(`token: ${token}`);
+      })
+      .catch(error => {
+        // error handling here
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // email of user's account used
+        const email = error.email;
+        // firebase auth auth credential type that was used
+        const credential = error.credential;
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log(email);
+        console.log(credential);
+      });
+  },
+  AuthStateChanged: () => {
+    auth.onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        firebase.auth.Auth.Persistence.SESSION;
+        console.log("FirebaseUser object below");
+        console.log(firebaseUser);
+        // firebase unique user id
+        console.log(`Firebase uid: ${firebaseUser.uid}`);
+        console.log(`displayName: ${firebaseUser.displayName}`);
+        logoutBtn.classList.remove("hide");
+        loginBtn.classList.add("hide");
+      } else {
+        console.log("not logged in");
+        logoutBtn.classList.add("hide");
+        loginBtn.classList.remove("hide");
+      }
+    });
+  }
+};
+
 // signs out then authenticated user
 $("#logout").on("click", event => {
-  auth.signOut();
+  firebaseAuth.signOut();
 });
 
-// test google sign in
+// Sign in through twitter
 $("#login").on("click", event => {
-  auth
-    .signInWithRedirect(provider)
-    .them(result => {
-      // this gives you a google access token. Used to access the google api
-      const token = result.credential.accessToken;
-      // the signed-in user info
-      const user = result.user;
-      console.log(`user: ${user}`);
-      console.log(`token: ${token}`);
-    })
-    .catch(error => {
-      // error handling here
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // email of user's account used
-      const email = error.email;
-      // firebase auth auth credential type that was used
-      const credential = error.credential;
-      console.log(errorCode);
-      console.log(errorMessage);
-      console.log(email);
-      console.log(credential);
-    });
+  firebaseAuth.signIn();
 });
 
-// real time authentication state listener
-auth.onAuthStateChanged(firebaseUser => {
-  if (firebaseUser) {
-    firebase.auth.Auth.Persistence.SESSION;
-    console.log("FirebaseUser object below");
-    console.log(firebaseUser);
-    // firebase unique user id
-    console.log(`Firebase uid: ${firebaseUser.uid}`);
-    console.log(`displayName: ${firebaseUser.displayName}`);
-    logoutBtn.classList.remove("hide");
-    loginBtn.classList.add("hide");
-  } else {
-    console.log("not logged in");
-    logoutBtn.classList.add("hide");
-    loginBtn.classList.remove("hide");
-  }
-});
+// listener for when authentication state changes
+firebaseAuth.AuthStateChanged();
